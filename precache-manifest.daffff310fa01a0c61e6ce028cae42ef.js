9559ee7e01a082b60a47a2b39c9d933c90e09482
Yash Gandhi <yashgandhi2020@gmail.com>
2020-04-15 03:37:12 +0530

    Updates

---
+++ precache-manifest.daffff310fa01a0c61e6ce028cae42ef.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ace5af30d3dd22de7af39858140137c1",
+    "url": "/calculator-react/index.html"
+  },
+  {
+    "revision": "d16cf8becc34ce0c9c90",
+    "url": "/calculator-react/static/css/main.04c3f985.chunk.css"
+  },
+  {
+    "revision": "43c4d6fc64b5b8885d2c",
+    "url": "/calculator-react/static/js/2.7c95cc0f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/calculator-react/static/js/2.7c95cc0f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d16cf8becc34ce0c9c90",
+    "url": "/calculator-react/static/js/main.977ca7bd.chunk.js"
+  },
+  {
+    "revision": "ec6ea1919e03c15d0bef",
+    "url": "/calculator-react/static/js/runtime-main.0e705f54.js"
+  }
+]);
